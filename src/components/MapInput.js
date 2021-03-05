01e7f69ec4d14d1988d7436591a864408dd12326
JingWang <jingwang9302@gmail.com>
2021-03-04 21:22:16 -0800

    FEAT - search and show result name as a list on map

--- src/components/MapInput.js
+++ src/components/MapInput.js
@@ -1,13 +1,8 @@
-import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Dimensions,
-} from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View, Dimensions } from "react-native";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { config } from "../../config";
+import useResults from "../hooks/useResults";
 
 const { height, width } = Dimensions.get("window");
 const ASPECT_RATIO = width / height;
@@ -23,7 +18,12 @@ const workPlace = {
   geometry: { location: { lat: 48.8496818, lng: 2.2940881 } },
 };
 
-const MapInput = ({ setRegion, setMarker }) => {
+const MapInput = ({ setRegion, setMarker, currentLocation }) => {
+  const [text, setText] = useState("");
+  const [getSearchResultApi, resultList, errorMessage] = useResults(
+    currentLocation
+  );
+  const searchResult = resultList.resultList.results;
   return (
     <GooglePlacesAutocomplete
       placeholder="Enter Location"
@@ -38,7 +38,7 @@ const MapInput = ({ setRegion, setMarker }) => {
           const location = details.geometry.location;
           const photoReference =
             details.photos && details.photos[0].photo_reference;
-          console.log(details.name);
+          // console.log(details.name);
           setRegion({
             latitude: location.lat,
             longitude: location.lng,
@@ -53,7 +53,7 @@ const MapInput = ({ setRegion, setMarker }) => {
             url: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoReference}&key=${config.PLACES_API_KEY}`,
           });
         } else {
-          console.log(details);
+          // console.log(details);
           setRegion({
             latitude: details.geometry.location.lat,
             longitude: details.geometry.location.lng,
@@ -68,16 +68,29 @@ const MapInput = ({ setRegion, setMarker }) => {
           });
         }
       }}
+      textInputProps={{
+        clearButtonMode: "always",
+        InputComp: TextInput,
+        onChangeText: (text) => {
+          setText(text);
+        },
+        onSubmitEditing: () => {
+          getSearchResultApi(text);
+        },
+      }}
       query={{
         key: config.PLACES_API_KEY,
         language: "en",
       }}
       nearbyPlacesAPI="GooglePlacesSearch"
-      debounce={200}
+      debounce={500}
       predefinedPlaces={[homePlace, workPlace]}
       currentLocation={true}
       currentLocationLabel="Current Location"
-    />
+    >
+      {searchResult.length > 0 &&
+        searchResult.map((res) => <Text>{res.name}</Text>)}
+    </GooglePlacesAutocomplete>
   );
 };
 
---
+++ src/hooks/useResults.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { config } from "../../config";
+
+export default (currentLocation) => {
+  const state = {
+    resultList: [],
+  };
+  const [resultList, setResultList] = useState(state.resultList);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const getSearchResult = (searchTerm) => {
+    const { latitude, longitude } = currentLocation;
+    const url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+    const location = `location=${latitude},${longitude}`;
+    const radius = "&radius=2000";
+    const type = `&keyword=${searchTerm}`;
+    const key = `&key=${config.PLACES_API_KEY}`;
+    const searchUrl = url + location + radius + type + key;
+
+    fetch(searchUrl)
+      .then((response) => response.json())
+      .then((result) => setResultList({ resultList: result }))
+      .catch((err) => {
+        setErrorMessage("Something went wrong:" + err);
+      });
+  };
+
+  return [getSearchResult, resultList, errorMessage];
+};
