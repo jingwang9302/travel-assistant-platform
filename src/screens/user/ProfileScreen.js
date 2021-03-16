373b99b2219a717cc6d12e0c1978649bca508969
Xiaoning Zhao <xiaoning.zhao@sjsu.edu>
2021-03-16 13:51:59 -0700

    Add edit profile

--- src/screens/user/ProfileScreen.js
+++ src/screens/user/ProfileScreen.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
-import {connect} from "react-redux";
-import {Avatar, Card, Icon} from "react-native-elements";
+import React, {useState} from 'react';
+import {ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {connect, useDispatch} from "react-redux";
+import {Avatar, Card, Icon, Input, Overlay} from "react-native-elements";
 import LoginAlertScreen from "./LoginAlertScreen";
+import axios from "axios";
+import {USER_SERVICE} from "../../config/urls";
+import {setProfile} from "../../redux/actions/user";
 
 const ProfileScreen = ({userProfile}) => {
 
@@ -10,6 +13,60 @@ const ProfileScreen = ({userProfile}) => {
         return (
             <LoginAlertScreen/>
         );
+    }
+    const dispatch = useDispatch();
+    const [visible, setVisible] = useState(false);
+    const [firstName, setFirstName] = useState(userProfile.firstName);
+    const [lastName, setLastName] = useState(userProfile.lastName);
+    const [address, setAddress] = useState(userProfile.address);
+    const [phone, setPhone] = useState(userProfile.phone);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const toggleOverlay = () => {
+        setVisible(!visible);
+    };
+
+    const saveProfile = () =>{
+        if (!firstName) {
+            setErrorMessage('Please fill First Name');
+            return;
+        }
+        if (!lastName) {
+            setErrorMessage('Please fill Last Name');
+            return;
+        }
+
+        axios({
+            method: 'put',
+            url: USER_SERVICE +'/profile',
+            headers: {
+                'Authorization': 'Bearer '+ userProfile.token
+            },
+            data: {
+                id: userProfile.id,
+                password:'0',
+                email: userProfile.email,
+                firstName: firstName,
+                lastName: lastName,
+                address: address,
+                phone: phone
+            },
+        })
+            .then(function (response) {
+                if(response.status === 200){
+                    setVisible(false);
+                    setErrorMessage('');
+                    dispatch(setProfile(response.data));
+                }
+            })
+            .catch(function (error) {
+                if(error.response.data.message === null){
+                    setErrorMessage(error.message);
+                }else{
+                    setErrorMessage(error.response.data.message);
+                }
+            });
+
     }
 
     return(
@@ -26,29 +83,103 @@ const ProfileScreen = ({userProfile}) => {
                             }
                             activeOpacity={0.7}
                             overlayContainerStyle={{backgroundColor: 'grey'}}/>
-                        <Text style={{fontSize: 20}}>{userProfile.firstName+' '+userProfile.lastName}</Text>
+                        <Text style={{fontSize: 20, fontWeight:'bold'}}>{userProfile.firstName+' '+userProfile.lastName}</Text>
                     </View>
                 </Card.Title>
                 <Card.Divider/>
-                <View style={{flexDirections: 'column', }}>
-                    <Text style={styles.cardTextStyle}>
-                        <Icon name='email' type = 'material-community' color='black'/>  {userProfile.email}
-                    </Text>
-                    <Text style={styles.cardTextStyle}>
-                        <Icon name='home-city' type = 'material-community' color='black'/>  {userProfile.address}
-                    </Text>
-                    <Text style={styles.cardTextStyle}>
-                        <Icon name='phone' type = 'material-community' color='black'/>  {userProfile.phone}
-                    </Text>
+                <View style={{flexDirections: 'column', paddingLeft:30, paddingRight:30}}>
+                    <View style={{flexDirection:'row', alignItems:'center'}}>
+                        <Icon name='email' type = 'material-community' color='black'/>
+                        <Text style={styles.cardTextStyle}>
+                            {userProfile.email}
+                        </Text>
+                    </View>
+                    <View style={{flexDirection:'row' , alignItems:'center'}}>
+                        <Icon name='home-city' type = 'material-community' color='black'/>
+                        <Text style={styles.cardTextStyle}>
+                            {userProfile.address}
+                        </Text>
+                    </View>
+                    <View style={{flexDirection:'row' , alignItems:'center'}}>
+                        <Icon name='phone' type = 'material-community' color='black'/>
+                        <Text style={styles.cardTextStyle}>
+                            {userProfile.phone}
+                        </Text>
+                    </View>
                 </View>
             </Card>
 
             <TouchableOpacity
                 style={styles.logoutButtonStyle}
                 activeOpacity={0.5}
-                onPress={()=>{alert('edit')}}>
+                onPress={()=>{setVisible(true)}}>
                 <Text style={styles.buttonTextStyle}>Edit Profile</Text>
             </TouchableOpacity>
+
+            <Overlay overlayStyle={styles.OverlayStyle} isVisible={visible} onBackdropPress={toggleOverlay}>
+                <View style={{width:300, margin:10}}>
+                    <ScrollView >
+                        <Input
+                            label={'Email'}
+                            style={styles.inputStyle}
+                            defaultValue={userProfile.email}
+                            disabled={true}
+                            leftIcon={
+                                <Icon name='email' size={24} type = 'material-community' color='grey'/>
+                            }
+                        />
+                        <Input
+                            label={'First Name'}
+                            style={styles.inputStyle}
+                            defaultValue={firstName}
+                            onChangeText={(text) => setFirstName(text)}
+                            leftIcon={
+                                <Icon name='account' size={24} type = 'material-community' color='grey'/>
+                            }
+                        />
+                        <Input
+                            label={'Last Name'}
+                            style={styles.inputStyle}
+                            defaultValue={lastName}
+                            onChangeText={(text) => setLastName(text)}
+                            leftIcon={
+                                <Icon name='account' size={24} type = 'material-community' color='grey'/>
+                            }
+                        />
+                        <Input
+                            label={'Address'}
+                            style={styles.inputStyle}
+                            defaultValue={address}
+                            onChangeText={(text) => setAddress(text)}
+                            leftIcon={
+                                <Icon name='home-city' size={24} type = 'material-community' color='grey'/>
+                            }
+                        />
+                        <Input
+                            label={'Phone'}
+                            style={styles.inputStyle}
+                            defaultValue={phone}
+                            onChangeText={(text) => setPhone(text)}
+                            leftIcon={
+                                <Icon name='phone' size={24} type = 'material-community' color='grey'/>
+                            }
+                        />
+                        <View>
+                            {errorMessage !== '' ? (
+                                <Text style={styles.errorTextStyle}>
+                                    {errorMessage}
+                                </Text>
+                            ) : null}
+                        </View>
+                        <TouchableOpacity
+                            style={styles.buttonStyle}
+                            activeOpacity={0.5}
+                            onPress={()=>{saveProfile()}}>
+                            <Text style={styles.buttonTextStyle}>SAVE</Text>
+                        </TouchableOpacity>
+                    </ScrollView>
+                </View>
+            </Overlay>
         </View>
 
     );
@@ -113,6 +244,19 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 18,
         padding: 30,
+    },
+    OverlayStyle: {
+        justifyContent: 'center',
+        alignContent: 'center',
+    },
+    inputStyle: {
+        paddingLeft: 15,
+        paddingRight: 15,
+    },
+    errorTextStyle: {
+        color: 'red',
+        textAlign: 'center',
+        fontSize: 14,
     },
 });
 
