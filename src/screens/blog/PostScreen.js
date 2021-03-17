5a6f5d956299f0159c1971bf841e8c1e07db33b1
Xiaoning Zhao <xiaoning.zhao@sjsu.edu>
2021-03-17 13:18:52 -0700

    Enable paging posts

--- src/screens/blog/PostScreen.js
+++ src/screens/blog/PostScreen.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {
-    FlatList, RefreshControl, View,
+    ActivityIndicator,
+    FlatList, RefreshControl, Text, View,
 } from 'react-native';
 import axios from "axios";
 import {BLOG_SERVICE} from "../../config/urls";
@@ -9,28 +10,40 @@ import Post from "../../components/Post";
 import {Icon} from "react-native-elements";
 import {Fab} from "native-base";
 
+const PAGE_SIZE = 5;
+
 const PostScreen = ({navigation}) => {
 
     const userProfile = useSelector(state => state.user);
 
     const [posts, setPosts] = useState([]);
-    const [refreshing, setRefreshing] = React.useState(false);
+    const [refreshing, setRefreshing] = useState(false);
+    const [pageNumber, setPageNumber] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const [isPageEnd, setIsPageEnd] = useState(false);
 
     useEffect(() => {
-        getUserPosts();
+        getUserPosts(0);
     },[]);
 
 
-    const getUserPosts = () => {
+    const getUserPosts = (page) => {
+        setLoading(true);
         axios({
             method: 'get',
-            url: BLOG_SERVICE +'/post/all/'+ userProfile.id,
+            url: BLOG_SERVICE +'/post/all/'+ userProfile.id +'/page/'+ page + '/size/' + PAGE_SIZE ,
             headers: {
                 'Authorization': 'Bearer '+ userProfile.token
             }
         })
             .then(function (response) {
-                setPosts(response.data);
+
+                if(response.data.length === 0){
+                    setIsPageEnd(true);
+                }else{
+                    setPosts(posts.concat(response.data));
+                }
+                setLoading(false);
             })
             .catch(function (error) {
                 if(error.response.data.message === null){
@@ -38,13 +51,16 @@ const PostScreen = ({navigation}) => {
                 }else {
                     console.log(error.response.data.message);
                 }
+                setLoading(false);
             });
     }
 
     const onRefresh = React.useCallback(() => {
         setRefreshing(true);
         setPosts([{id:0}]);
-        getUserPosts();
+        getUserPosts(0);
+        setPageNumber(1);
+        setIsPageEnd(false);
         setRefreshing(false)
     }, []);
 
@@ -52,18 +68,47 @@ const PostScreen = ({navigation}) => {
         <Post postId={item.id} />
     );
 
+    const renderFooter = () => {
+        if (isPageEnd){
+            return (
+                <View style={{alignItems:'center'}}>
+                    <Text style={{fontSize:15}}>No more data. 别撸啦</Text>
+                </View>
+            )
+        }
+        if (!loading) return null;
+        return (
+            <View style={{alignItems:'center'}}>
+                <ActivityIndicator
+                    style={{ color: '#000000' }}
+                />
+            </View>
+        );
+    };
+
+    const handleLoadMore = () => {
+        if (!loading && !isPageEnd) {
+            setPageNumber(pageNumber+1);
+            getUserPosts(pageNumber);
+        }
+    };
+
 
     return(
         <View style={{flex: 1}}>
             <FlatList
                 data={posts}
                 renderItem={renderItem}
+                keyExtractor={(item) => item.id.toString()}
                 refreshControl={
                     <RefreshControl
                         refreshing={refreshing}
                         onRefresh={onRefresh}
                     />
                 }
+                ListFooterComponent={renderFooter}
+                onEndReachedThreshold={0.01}
+                onEndReached={()=>handleLoadMore()}
             />
 
             {/*<ScrollView>*/}
