428ffa77c845ced0f1bf9b52f65c3ac48d0867cd
Tracey Wang <41496036+jingwang9302@users.noreply.github.com>
2021-04-02 00:26:01 -0700

    Refined result screen, fixed show route only show on first press, init navigation screen  (#4)
    * Refined result screen
    
    * Added draw route from one place to another
    
    * Updated navigation, replaced hardcoded location
    
    * Bug fixed -- route only show when pressed for the first time
    
    * Styled places card, added route distance and estimate time
    
    * Seperate show route feature from map screen, added useDirection hook
    
    * Added navigation screen

--- src/screens/maps/SearchScreen.js
+++ src/screens/maps/SearchScreen.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Dimensions,
   Image,
+  TouchableOpacity,
 } from "react-native";
 import MapView, { Marker, PROVIDER_GOOGLE, Callout } from "react-native-maps";
 import Polyline from "@mapbox/polyline";
@@ -13,6 +14,7 @@ import { Card, ListItem, Button, Icon } from "react-native-elements";
 import MapInput from "../../components/MapInput";
 import ResultList from "./ResultsList";
 import { useCurrentLocation } from "../../hooks/useCurrentLocation";
+import useDirection from "../../hooks/useDirection";
 import { config } from "../../../config";
 
 const { height, width } = Dimensions.get("window");
@@ -26,7 +28,7 @@ const SearchScreen = ({ navigation }) => {
   let [region, setRegion] = useState(null);
   const [marker, setMarker] = useState([]);
   const [curMarker, setCurMarker] = useState(null);
-  const [navigationInfo, setNavigationInfo] = useState(null);
+  const { navigationInfo, getDirections } = useDirection();
   const { currentLocation, loading, error } = useCurrentLocation();
   if (!currentLocation) {
     return (
@@ -43,34 +45,8 @@ const SearchScreen = ({ navigation }) => {
     longitudeDelta: LONGITUDE_DELTA,
   };
 
-  const getDirections = async (startLoc, desLoc) => {
-    try {
-      const resp = await fetch(
-        `https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${desLoc}&key=${config.DIRECTION_API_KEY}`
-      );
-      const respJson = await resp.json();
-      const response = respJson.routes[0];
-      const distanceTime = response.legs[0];
-      const distance = distanceTime.distance.text;
-      const time = distanceTime.duration.text;
-      const points = Polyline.decode(
-        respJson.routes[0].overview_polyline.points
-      );
-      const coords = points.map((point) => {
-        return {
-          latitude: point[0],
-          longitude: point[1],
-        };
-      });
-      setNavigationInfo({ coords, distance, time });
-      // console.log(navigationInfo);
-      console.log(coords);
-    } catch (error) {
-      console.log("Error: ", error);
-    }
-  };
-
   const mergeCoods = (desLocation) => {
+    console.log("merge coords");
     if (desLocation) {
       const { desLatitude, desLongitude } = desLocation;
       const hasStartAndEnd =
@@ -78,11 +54,11 @@ const SearchScreen = ({ navigation }) => {
       if (hasStartAndEnd) {
         const concatStart = `${currentLocation.latitude},${currentLocation.longitude}`;
         const concatEnd = `${desLatitude},${desLongitude}`;
-        console.log("concat location", concatEnd);
         getDirections(concatStart, concatEnd);
       }
     }
   };
+  console.log(navigationInfo);
   return (
     <View>
       <MapView
@@ -92,10 +68,10 @@ const SearchScreen = ({ navigation }) => {
         showsMyLocationButton={true}
         initialRegion={initRegion}
         region={region || initRegion}
-        onPress={(e) => {
-          if (e.nativeEvent.action) return;
-          setMarker([e.nativeEvent.coordinate]);
-        }}
+        // onPress={(e) => {
+        //   if (e.nativeEvent.action) return;
+        //   setMarker([e.nativeEvent.coordinate]);
+        // }}
       >
         {navigationInfo &&
           navigationInfo.coords &&
@@ -128,8 +104,8 @@ const SearchScreen = ({ navigation }) => {
                 tooltip
               >
                 <View style={styles.bubble}>
-                  <Text>{item.title}</Text>
-                  <Text>{item.address}</Text>
+                  <Text style={{ fontWeight: "bold" }}>{item.title}</Text>
+                  <Text style={{ fontSize: 12 }}>{item.address}</Text>
                 </View>
               </Callout>
             </Marker>
@@ -138,29 +114,55 @@ const SearchScreen = ({ navigation }) => {
       {curMarker && typeof curMarker.url === "string" && (
         <View
           style={{
-            backgroundColor: "red",
+            backgroundColor: "white",
             position: "absolute",
-            top: 400,
+            top: height - 392,
             alignSelf: "center",
           }}
         >
-          <Image
-            source={{
-              uri: curMarker.url,
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("Result", { result: curMarker });
             }}
-            style={{
-              width: width * 0.95,
-              alignSelf: "center",
-              height: height * 0.15,
-            }}
-          />
+          >
+            <Image
+              source={{
+                uri: curMarker.url,
+              }}
+              style={{
+                width: width,
+                alignSelf: "center",
+                height: height * 0.15,
+              }}
+            />
+          </TouchableOpacity>
+
           <View>
-            <Text style={{ backgroundColor: "white", fontSize: 20 }}>
-              {curMarker.title}
+            <Text
+              style={{
+                backgroundColor: "white",
+                fontSize: 18,
+                fontWeight: "bold",
+              }}
+            >
+              {`${curMarker.title}`}
             </Text>
-            <Text style={{ backgroundColor: "white", fontSize: 20 }}>
-              {curMarker.title}
+            <Text style={{ backgroundColor: "white", fontSize: 15 }}>
+              {curMarker.address}
             </Text>
+            {navigationInfo && (
+              <Text style={{ fontSize: 15 }}>
+                {`Estimate time: ${navigationInfo.time}        Distance: ${navigationInfo.distance}`}
+              </Text>
+            )}
+            <Button
+              onPress={() => {
+                navigation.navigate("Navigation");
+              }}
+              icon={<Icon name="arrow-right" size={20} color="white" />}
+              iconRight
+              title="Start Navigation"
+            />
           </View>
         </View>
       )}
@@ -181,23 +183,57 @@ const SearchScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   mapInput: { position: "absolute", width: width },
   map: { height: "100%" },
-  button: {
-    width: 80,
-    paddingHorizontal: 12,
-    alignItems: "center",
-    marginHorizontal: 10,
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    marginVertical: 20,
-    backgroundColor: "transparent",
-  },
   bubble: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: "rgb(255,255,255)",
     paddingHorizontal: 18,
     paddingVertical: 12,
     borderRadius: 20,
+    width: 160,
+    height: 80,
+    opacity: 0.8,
+  },
+  scrollView: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    paddingVertical: 10,
+  },
+
+  textContent: {
+    flex: 2,
+    padding: 10,
+  },
+  cardtitle: {
+    fontSize: 12,
+    // marginTop: 5,
+    fontWeight: "bold",
+  },
+  cardDescription: {
+    fontSize: 12,
+    color: "#444",
+  },
+  card: {
+    // padding: 10,
+    elevation: 2,
+    backgroundColor: "#FFF",
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    marginHorizontal: 10,
+    shadowColor: "#000",
+    shadowRadius: 5,
+    shadowOpacity: 0.3,
+    shadowOffset: { x: 2, y: -2 },
+    height: CARD_HEIGHT,
+    width: CARD_WIDTH,
+    overflow: "hidden",
+  },
+  cardImage: {
+    flex: 3,
+    width: "100%",
+    height: "100%",
+    alignSelf: "center",
   },
   scrollView: {
     position: "absolute",
