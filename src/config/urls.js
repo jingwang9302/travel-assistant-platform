6ae7370ece592334ba5d02c5ae6ead0dbad4188d
jun zhang <jun.zhang01@sjsu.edu>
2021-04-22 22:54:54 -0700

    Update photo URL

--- src/config/urls.js
+++ src/config/urls.js
@@ -9,20 +9,28 @@ export const PLAN_BASE_URL = "https://travel-plan-service.wn.r.appspot.com/";
 /** User-related service */
 export const USER_SERVICE = BASE_URL + "/user/api/user";
 export const LOGIN_URL = BASE_URL + "/login";
-export const USER_BASIC_PROFILE_BY_USERID_URL = USER_SERVICE + "/profile/basic/";
+export const USER_BASIC_PROFILE_BY_USERID_URL =
+  USER_SERVICE + "/profile/basic/";
 
 /** Plan related service */
-export const PLAN_SERVICE = "https://travel-plan-service.wn.r.appspot.com/v1/travelplan/";
+export const PLAN_SERVICE =
+  "https://travel-plan-service.wn.r.appspot.com/v1/travelplan/";
 export const NOTIFICATION_SERVICE = BASE_URL + "/notification/api/notification";
-export const NOTIFICATION_WEBSOCKET_SERVICE = BASE_URL + "/notification-ws/notification";
+export const NOTIFICATION_WEBSOCKET_SERVICE =
+  BASE_URL + "/notification-ws/notification";
 export const BLOG_SERVICE = BASE_URL + "/user/api/blog";
 export const UPLOAD_IMAGE_URL = BASE_URL + "/user";
-export const GET_FINISHED_TRAVEL_PLANS_BY_USER_ID = PLAN_SERVICE + "read_finished/";
+export const GET_FINISHED_TRAVEL_PLANS_BY_USER_ID =
+  PLAN_SERVICE + "read_finished/";
+export const GET_A_SINGLE_PLAN_BY_ID = PLAN_SERVICE + "read/";
 
 /** Group related service */
-export const GROUP_SERVICE = "https://travel-group-service.wn.r.appspot.com/v1/travelgroup/";
-export const GET_ALL_GROUPS_USER_BELONGS_BY_USERID = GROUP_SERVICE + "read/groups_in/";
+export const GROUP_SERVICE =
+  "https://travel-group-service.wn.r.appspot.com/v1/travelgroup/";
+export const GET_ALL_GROUPS_USER_BELONGS_BY_USERID =
+  GROUP_SERVICE + "read/groups_in/";
 /** google cloud storage */
-export const GCS_URL = "https://storage.googleapis.com/travel-group-service-bucket/";
+export const GCS_URL =
+  "https://storage.googleapis.com/travel-group-plan-image-bucket/";
 
 /** Travel related service */
